1a9d372f7a1c757aadb92b33cb3e193cd2293846
Shekhar Kale <skale.ui@gmail.com>
2024-04-22 20:56:38 -0700

    webhooks integration

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -5,9 +5,9 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true},
   email: { type: String, required: true, unique: true },
   username: { type: String, required: false, unique: true },
- // photo: { type: String, required: false },
- // firstName: { type: String, requried:false },
- // lastName: { type: String, required:false },
+ photo: { type: String, required: false },
+ firstName: { type: String, requried:false },
+ lastName: { type: String, required:false },
  // planId: { type: Number, default: 1 },
  // creditBalance: { type: Number, default: 10},
 });
